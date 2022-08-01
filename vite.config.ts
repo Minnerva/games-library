a3d8c680411a911cdaabb04b8de1bfa8bc28f0ca
Min <potchara40@hotmail.com>
2022-08-01 15:38:39 +0700

    add .env file and make Vite start the port as declare in .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ``)
+
+  return {
+    server: {
+      port: parseInt(env.VITE_PORT)
+    },
+    plugins: [svelte()]
+  }
 })
+
